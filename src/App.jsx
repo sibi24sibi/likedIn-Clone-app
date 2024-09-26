e56042cc88c897564d56f496aa8cca3a2092982b
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-09-26 13:07:47 +0530

    feat:installed fontawesome icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
