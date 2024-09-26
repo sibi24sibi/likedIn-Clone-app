dd2aeb74dfaa28d5adf54db192d30f58d2325a59
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-09-26 10:57:19 +0530

    featt:added flowbite ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./node_modules/flowbite/**/*.js",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
