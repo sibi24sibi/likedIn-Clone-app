e2a5ddbe3d2be1a037574a78676e7d84ee4754cd
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-09-28 19:48:29 +0530

    bug:made position change in footer

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
       <SigninForm />
 
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import linkedInLogo from "../assets/LinkedIn-Logo.png";
 
 function Footer() {
   return (
-    <footer className="absolute w-full bottom-0 bg-gray-100 text-gray-600 py-10">
+    <footer className="relative w-full bottom-0 bg-gray-100 text-gray-600 py-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Top Section */}
         <div className="grid grid-cols-1 md:grid-cols-5 gap-8 mb-8">
