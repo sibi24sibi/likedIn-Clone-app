eae76797377e263b7135979cfbe3ae4328938b0d
karan-agarwal-01 <karanagarwal858@gmail.com>
2024-10-04 15:21:21 +0530

    profile section with edit profile form

--- src/Components/ProfileSection.jsx
+++ src/Components/ProfileSection.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaPencilAlt } from 'react-icons/fa';
 import { FiPlusCircle } from 'react-icons/fi';
 import { GoDotFill } from 'react-icons/go';
 import { HiOutlineOfficeBuilding } from 'react-icons/hi';
 import { IoNotifications, IoSchoolOutline } from 'react-icons/io5';
 import { MdVerifiedUser } from 'react-icons/md';
+import Modal from './Modal/Modal';
+import ProfileForm from './ProfileForm';
 
 function ProfileSection() {
+
+  const [open, setOpen] = useState(false)
+
   return (
+    <>
       <div className='bg-gray-100 h-auto max-w-max rounded-xl my-10 ml-20 flex flex-col border border-grey'>
         <img className='relative h-[210px] w-[770px] rounded-t-xl object-cover' src='src\assets\images\cover2.webp' alt='cover-photo' />
         <div>
@@ -32,12 +39,6 @@ function ProfileSection() {
               <a href='/' className='text-[14px] text-sky-600 font-medium'>Contact info</a>
             </div>
             <a href='/' className='text-[14px] text-sky-600 font-medium'>500+ connections</a>
-            <div className='py-3'>
-              <button className='flex gap-1 justify-center items-center bg-blue-500 rounded-lg text-white font-medium p-1.5 w-24 hover:bg-blue-950' type='button'>
-              <FiPlusCircle />
-              <p className='text-sm'>Connect</p>
-              </button>
-            </div>
           </div>
           <div className='mr-10 mt-12'>
             <div className='flex justify-start items-center gap-2.5'>
@@ -50,7 +51,21 @@ function ProfileSection() {
             </div>
           </div>
         </div>
+        <div className='flex justify-between ml-6 my-2 mb-4'>
+          <button className='flex gap-1 justify-center items-center bg-blue-500 rounded-lg text-white font-medium p-2 w-24 hover:bg-blue-950' type='button'>
+            <FiPlusCircle />
+              <p className='text-sm'>Connect</p>
+          </button>
+          <button className='flex gap-1 justify-center items-center bg-blue-500 rounded-lg text-white font-medium p-2 w-20 hover:bg-blue-950 mr-6' type='button' onClick={() => setOpen(true)}>
+            <FaPencilAlt />
+              <p className='text-sm'>Edit</p>
+          </button>
+        </div>
       </div>
+      <Modal open={open} onclose={() => setOpen(false)}>
+        <ProfileForm />
+      </Modal>
+    </>
   )
 }
 
