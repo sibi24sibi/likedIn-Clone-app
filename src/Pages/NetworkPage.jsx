44cde4230ba1f8d35dd89d5a0b386c46ab0f2350
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-10-13 21:18:38 +0530

    fixed:updated profile section -5

--- src/Pages/NetworkPage.jsx
+++ src/Pages/NetworkPage.jsx
@@ -3,6 +3,8 @@ import ConnectModel from "../Components/ConnectModel";
 import { collection, onSnapshot } from "firebase/firestore";
 import { firestore } from "../Firebase";
 
+
+
 export const NetworkPage = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -24,7 +26,7 @@ export const NetworkPage = () => {
   }, []);
 
   return (
-    <div className="md:p-10 max-w-sm md:max-w-none bg-gray-100 min-h-screen ">
+    <div className="md:p-10  md:max-w-none bg-gray-100 min-h-screen ">
       <div className="max-w-5xl mx-auto ">
         <h3 className="text-4xl font-semibold m-6 uppercase text-center">People You May Know</h3>
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-4 p-4 ">
