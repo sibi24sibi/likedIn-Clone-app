dc177ade4ed7e5b3a64302efc0f9885b489eedd0
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2025-01-14 10:30:22 +0530

    fixed follow notification -10

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(import.meta.env.VITE_NONE === "production"
-      ? { cssnano: {} }
-      : {}),
   },
-};
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   darkMode: "class",
   purge: [
     "./src/**/*.{js,jsx,ts,tsx}", // your React files
   ],
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // your React files
-  ],
+
 
   plugins: [],
 };
