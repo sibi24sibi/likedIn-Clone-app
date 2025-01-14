94d3403a19c428c4e16ec833629bd80732719fab
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2025-01-14 09:54:28 +0530

    fixed follow notification -4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}", // your React files
+  ],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // your React files
   ],
