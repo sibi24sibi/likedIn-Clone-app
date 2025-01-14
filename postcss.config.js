7a4b2ccd1561adc628528b98c48d846e15a4d2eb
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2025-01-14 10:01:56 +0530

    fixed follow notification -5

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
-}
+});
